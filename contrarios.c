11dfb6c4a3064dac0301f2783cded29d6a06c1c0
Pedro Schneider <143662630+PedrooSchneiderr@users.noreply.github.com>
2024-08-19 12:01:10 -0300

    Update contrarios.c

--- contrarios.c
+++ contrarios.c
@@ -13,3 +13,6 @@ int main(){
     printf("\n");
     return 0;
 }
+/*Esse código em C lê 10 números digitados pelo usuário e, em seguida, 
+imprime esses números na ordem inversa em que foram inseridos. 
+Ou seja, ele exibe os números na sequência inversa da entrada original. */
