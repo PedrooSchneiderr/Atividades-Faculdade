0f3021cd6e080f045a5afd9407cffd0f5a027814
Pedro Schneider <143662630+PedrooSchneiderr@users.noreply.github.com>
2024-08-19 11:56:13 -0300

    Update contaenorme.c

--- contaenorme.c
+++ contaenorme.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-double resultado = pow(4, 3) + 7 - (3/2) * 8 + sqrt(2);
+int main() {
+    double resultado = pow(4, 3) + 7 - (3.0 / 2) * 8 + sqrt(2);
     printf("%f", resultado);
-    return 0;
-
-
-
+    return 0; 
 }
+ // Calcula o resultado da expressão: 4^3 + 7 - (3/2) * 8 + sqrt(2)
